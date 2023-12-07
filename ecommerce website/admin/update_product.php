751852e99f74c7d4e2452702d41701be966b2698
vinalu <84574823+vinalu@users.noreply.github.com>
2023-12-07 12:19:10 +0700

    Update update_product.php

--- ecommerce website/admin/update_product.php
+++ ecommerce website/admin/update_product.php
@@ -92,7 +92,7 @@ if(isset($_POST['update'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>update product</title>
+   <title>Perbarui Produk</title>
 
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
@@ -105,7 +105,7 @@ if(isset($_POST['update'])){
 
 <section class="update-product">
 
-   <h1 class="heading">update product</h1>
+   <h1 class="heading">perbarui produk</h1>
 
    <?php
       $update_id = $_GET['update'];
@@ -129,43 +129,32 @@ if(isset($_POST['update'])){
             <img src="../uploaded_img/<?= $fetch_products['image_03']; ?>" alt="">
          </div>
       </div>
-      <span>update name</span>
-      <input type="text" name="name" required class="box" maxlength="100" placeholder="enter product name" value="<?= $fetch_products['name']; ?>">
-      <span>update price</span>
-      <input type="number" name="price" required class="box" min="0" max="9999999999" placeholder="enter product price" onkeypress="if(this.value.length == 10) return false;" value="<?= $fetch_products['price']; ?>">
-      <span>update details</span>
+      <span>nama produk</span>
+      <input type="text" name="name" required class="box" maxlength="100" placeholder="masukkan nama produk" value="<?= $fetch_products['name']; ?>">
+      <span>harga produk</span>
+      <input type="number" name="price" required class="box" min="0" max="9999999999" placeholder="masukkan harga produk" onkeypress="if(this.value.length == 10) return false;" value="<?= $fetch_products['price']; ?>">
+      <span>detail produk</span>
       <textarea name="details" class="box" required cols="30" rows="10"><?= $fetch_products['details']; ?></textarea>
-      <span>update image 01</span>
+      <span>gambar 1</span>
       <input type="file" name="image_01" accept="image/jpg, image/jpeg, image/png, image/webp" class="box">
-      <span>update image 02</span>
+      <span>gambar 2</span>
       <input type="file" name="image_02" accept="image/jpg, image/jpeg, image/png, image/webp" class="box">
-      <span>update image 03</span>
+      <span>gambar 3</span>
       <input type="file" name="image_03" accept="image/jpg, image/jpeg, image/png, image/webp" class="box">
       <div class="flex-btn">
-         <input type="submit" name="update" class="btn" value="update">
-         <a href="products.php" class="option-btn">go back</a>
+         <input type="submit" name="update" class="btn" value="perbarui">
+         <a href="products.php" class="option-btn">kembali</a>
       </div>
    </form>
    
    <?php
          }
       }else{
-         echo '<p class="empty">no product found!</p>';
+         echo '<p class="empty">produk tidak ditemukan</p>';
       }
    ?>
 
 </section>
-
-
-
-
-
-
-
-
-
-
-
 
 <script src="../js/admin_script.js"></script>
    
